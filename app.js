66615bd0d2a8535191b85f684e9ff42ab3aef410
Gabriel Cunha <cunhagabriel975@gmail.com>
2025-03-22 19:36:18 -0300

    Commit important

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById('footer-placeholder').innerHTML = data;});
- 
-fetch('navbar.html')
-   .then(response => response.text())
-   .then(data => {
-    document.getElementById('navbar-placeholder').innerHTML = data;});
+
+
+    const links = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('section');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            // Obtém o alvo da seção
+            const target = link.getAttribute('data-target');
+
+            // Esconde todas as seções e remove a classe "active"
+            sections.forEach(section => {
+                section.classList.remove('active');
+            });
+
+            document.getElementById(target).classList.add('active');
+
+        history.pushState(null, '', target);
+
+
+        sections.forEach(section => section.style.display = 'none');
+        document.getElementById(target).style.display = 'block';
+        });
+    });
